dfa719eefab8fe2f9292c22e16779833d7dab212
lamerushman <kirill.puhalskiy@gmail.com>
2015-09-10 17:04:03 +0300

    new page objects  and some sample tests

--- screens/bucket_list.rb
+++ screens/bucket_list.rb
@@ -1,4 +1,9 @@
+require 'appium_lib'
+require 'rubygems'
+
 class BucketList < CommonScreen
+
+
   BUCKETLISTTITTLE = {:IOS => {uiautomation: '.navigationBars()[0].staticTexts()[0]'}}
 
   NOITEMSTEXT = {:IOS => {uiautomation: '.staticTexts()[0]'},
@@ -17,10 +22,62 @@ class BucketList < CommonScreen
                   :ANDROID => {id: '.1'}}
   FILTER = {:IOS => {uiautomation: '.navigationBars()[0].buttons()[3]'},
             :ANDROID => {id: '.1'}}
+  TABS = {:IOS => {uiautomation: '.scrollView()[1]'}}
+
+  BLTITLEADD = {:IOS => {uiautomation: '.textFields()[0]'},
+                :ANDROID => {id: '.1'}}
+  CREATEBLDONE = {:IOS => {uiautomation: '.navigationBars()[0].buttons()[2]'},
+                  :ANDROID => {id: '.1'}}
+  CREATEBLCLOSE = {:IOS => {uiautomation: '.navigationBars()[0].buttons()[1]'},
+                  :ANDROID => {id: '.1'}}
+
+  #BL LIST NAVIGATION
+
+  # self.cells = [
+  #     {:IOS =>{uiautomation: '.collectionViews()[0].collectionCells[0]'},
+  #          :ANDROID =>{id: '.1'}},
+  #     {:IOS =>{uiautomation: '.collectionViews()[0].collectionCells[1]'},
+  #          :ANDROID =>{id: '.1'}}
+  # ]
+
+  # Navigate between tabs of BL on IPHONE, could be different on different devices
+
+
+  def tap_activities_ios
+    @driver.execute_script 'mobile: tap', :x => 150, :y => 64
+  end
+
+  def tap_locations_ios
+    @driver.execute_script 'mobile: tap', :x => 0, :y => 64
+  end
+
+  def tap_dining_ios
+    @driver.execute_script 'mobile: tap', :x => 276, :y => 64
+  end
+
+  def additem
+    click(ADDBLIPOPTOP)
+    enter("additem_defaultmethod", BLTITLEADD)
+    click(CREATEBLDONE)
+    click(CREATEBLCLOSE)
+  end
+
+  def check_bl_presence(number)
+    text_include?("additem_defaultmethod",bl_cell_getter(number))
+  end
+
+  def enter_bl_details(number)
+    click(bl_cell_getter(number))
+  end
+
+  def bl_cell_getter(number)
+    cell = { :IOS => { uiautomation: ".collectionViews()[0].cells()[#{number}]."} }
+  end
+
+
 
 
   def ui_elements
-    #yield BUCKETLISTTITTLE
     yield NOITEMSTEXT
     yield ADDITEMTEXT
     yield ADDITEMARROW
